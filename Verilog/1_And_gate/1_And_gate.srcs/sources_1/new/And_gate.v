5486b15527d4949e4e8d720ab0f7b68124834083
irlalan <momoriallan@gmail.com>
2025-09-03 21:00:37 +0100

    updated file structure

---
+++ Verilog/1_And_gate/1_And_gate.srcs/sources_1/new/And_gate.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 01.09.2025 19:25:14
+// Design Name:
+// Module Name: And_gate
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module And_gate (
+    input  A,
+    B,
+    output O
+);
+    assign O = A & B;
+endmodule
